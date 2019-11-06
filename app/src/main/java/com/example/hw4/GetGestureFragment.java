5a8d03bf71d072de843769a5e801069a3dfe61e9
root <root@AHUNTE4-LT.localdomain>
2019-11-06 09:48:04 -0500

    added colors and fragment

---
+++ app/src/main/java/com/example/hw4/GetGestureFragment.java
@@ -0,0 +1,31 @@
+package com.example.hw4;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GetGestureFragment extends Fragment {
+
+
+    public GetGestureFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_get_gesture, container, false);
+    }
+
+}
